2a4af418d58b63ef4dd929f564e9e3ace74241be
Jazmin M <jazminmerckel@gmail.com>
2019-09-09 19:10:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,7 +6,7 @@ def display_board(board)
 board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 
   puts " #{board[0]} | #{board[1]} | #{board[2]}  "
-  puts(rows[1]).to eq("-----------")
+  puts (rows[1]).to eq("-----------")
   puts(rows[2]).to eq("   |   |   ")
   puts(rows[3]).to eq("-----------")
   puts(rows[4]).to eq("   |   |   ")

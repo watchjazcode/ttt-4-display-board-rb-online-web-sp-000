52704e50b49cafc402a6f3aa83cd4f0c91523de8
Jazmin M <jazminmerckel@gmail.com>
2019-09-09 18:56:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 display_board(board)
 
 puts "#{rows[0]}"

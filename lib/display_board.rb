2f24117a3c168b6aad587cf0168f45779a3dbd57
Jazmin M <jazminmerckel@gmail.com>
2019-09-09 19:11:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,8 +7,8 @@ board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 
   puts " #{board[0]} | #{board[1]} | #{board[2]}  "
   puts " #{board[0]} | #{board[1]} | #{board[2]}  "
+  puts " #{board[3]} | #{board[4]} | #{board[5]}  "
   puts " #{board[0]} | #{board[1]} | #{board[2]}  "
-  puts " #{board[0]} | #{board[1]} | #{board[2]}  "
-  puts " "
+  puts " #{board[6]} | #{board[7]} | #{board[8]}  "
 
 end

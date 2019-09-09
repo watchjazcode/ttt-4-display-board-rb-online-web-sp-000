8ef1a01d5bf497f2de9b51f068a5db61c39a111e
Jazmin M <jazminmerckel@gmail.com>
2019-09-09 19:17:25 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -15,3 +15,16 @@ display_board
 
 end
 
+def display_board(board)
+
+board = [" ", " ", " ", " ", " X ", " ", " ", " ", " "]
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]}  "
+  puts " ----------- "
+  puts " #{board[3]} | #{board[4]} | #{board[5]}  "
+  puts " -----------  "
+  puts " #{board[6]} | #{board[7]} | #{board[8]}  "
+
+display_board
+
+end

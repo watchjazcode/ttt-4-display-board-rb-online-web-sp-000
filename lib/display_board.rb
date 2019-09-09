2dc770e06757c7248e987facc7eea098adee768a
Jazmin M <jazminmerckel@gmail.com>
2019-09-09 19:21:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,32 +3,6 @@
 
 def display_board(board)
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-  puts " #{board[0]} | #{board[1]} | #{board[2]}  "
-  puts " ----------- "
-  puts " #{board[3]} | #{board[4]} | #{board[5]}  "
-  puts " -----------  "
-  puts " #{board[6]} | #{board[7]} | #{board[8]}  "
-
-end
-
-def display_board(board)
-
-board = [" ", " ", " ", " ", " X ", " ", " ", " ", " "]
-
-  puts " #{board[0]} | #{board[1]} | #{board[2]}  "
-  puts " ----------- "
-  puts " #{board[3]} | #{board[4]} | #{board[5]}  "
-  puts " -----------  "
-  puts " #{board[6]} | #{board[7]} | #{board[8]}  "
-
-end
-
-def display_board(board)
-
-board = [" O ", " ", " ", " ", " ", " ", " ", " ", " "]
-
   puts " #{board[0]} | #{board[1]} | #{board[2]}  "
   puts " ----------- "
   puts " #{board[3]} | #{board[4]} | #{board[5]}  "
